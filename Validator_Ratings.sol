921bc24e4483b9dcfbdc931fca89240333931108
rev97 <89413538+rev97@users.noreply.github.com>
2023-04-28 22:22:37 -0400

    removed syntax errors

--- Validator_Ratings.sol
+++ Validator_Ratings.sol
@@ -9,7 +9,6 @@ pragma solidity >=0.7.0 <0.9.0;
 contract ValidatorRatings {
 
     uint256 ValidationAccuracy;
-    uint256 
     struct MlPeer {
         string name;
         uint256 m_rating;
@@ -30,16 +29,16 @@ contract ValidatorRatings {
     }
     
     function retrieve_sr() public view returns (SiteReviewer[] memory){
-        return mlpeers; //retrieve tuple of all sitereviewers
+        return sitereviewers; //retrieve tuple of all sitereviewers
     }
 
-    function addmlpeer(string memory _name, string memory _m_rating) public {
+    function addmlpeer(string memory _name, uint256  _m_rating) public {
         mlpeers.push(MlPeer(_name, _m_rating)); //append to  MlPeer[] array
-        nameTomrating[_name] = _m_ratingr; //use name to get phone number
+        nameTomrating[_name] = _m_rating; //use name to get phone number
     }
-    function addmlpeer(string memory _name, string memory _m_rating) public {
-        mlpeers.push(MlPeer(_name, _m_rating)); //append to  MlPeer[] array
-        nameTomrating[_name] = _m_ratingr; //use name to get phone number
+    function addspreviwers(string memory _name, uint256 _s_rating) public {
+        mlpeers.push(MlPeer(_name, _s_rating)); //append to  MlPeer[] array
+        nameTomrating[_name] = _s_rating; //use name to get phone number
     }
     
 }
